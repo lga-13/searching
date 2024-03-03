5986b0da402e5556baddf95fb8ec450c719ebec6
glazarev <gleb.lazarev20@yandex.ru>
2024-03-03 07:44:45 +0300

    Типизирован валидатор

--- src/utils/field_validator.ts
+++ src/utils/field_validator.ts
@@ -1,38 +1,38 @@
 export const Validator = {
-    validateName: function(name) {
-        const re = /^[А-ЯA-Z][А-Яа-яA-Za-z\-]*$/;
+    validateName: function(name: string): boolean {
+        const re: RegExp = /^[А-ЯA-Z][А-Яа-яA-Za-z\-]*$/;
         console.log("- Имя должно начинаться с заглавной буквы\n" +
                     "- Имя может содержать только буквы и дефисы");
         return re.test(name);
     },
-    validateLogin: function(login) {
-        const re = /^[a-zA-Z][a-zA-Z0-9_-]{2,19}$/;
+    validateLogin: function(login: string): boolean {
+        const re: RegExp = /^[a-zA-Z][a-zA-Z0-9_-]{2,19}$/;
         console.log("- Логин должен начаться с алфавитного символа\n" +
                     "- Логин должен содержать от 2 до 19 символов\n" +
                     "- Логин может содержать только алфавитные символы, цифры, подчеркивания и дефисы");
         return re.test(login);
     },
-    validateEmail: function(email) {
-        const re = /^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+    validateEmail: function(email: string): boolean {
+        const re: RegExp = /^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]+$/;
         console.log("- Электронная почта должна быть формата text@text.text\n" +
                     "- Могут использоваться только алфавитные символы, цифры, подчеркивания и дефисы в адресе электронной почты");
         return re.test(email);
     },
-    validatePassword: function(password) {
-        const re = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,40}$/;
+    validatePassword: function(password: string): boolean {
+        const re: RegExp = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,40}$/;
         console.log("- Длина пароля должна быть от 8 до 40 символов\n" +
                     "- Пароль должен содержать хотя бы одну цифру\n" +
                     "- Пароль должен содержать хотя бы одну заглавную букву\n" +
                     "- Пароль может содержать только латинские буквы и цифры (никаких специальных символов или пробелов)");
         return re.test(password);
     },
-    validatePhone: function(phone) {
-        const re = /^\+?\d{10,15}$/;
+    validatePhone: function(phone: string): boolean {
+        const re: RegExp = /^\+?\d{10,15}$/;
         console.log("- Номер телефона должен содержать от 10 до 15 цифр\n" +
                     "- Номер телефона может начинаться с ведущего '+', но это не обязательно");
         return re.test(phone);
     },
-    validateMessage: function(message) {
+    validateMessage: function(message: string): boolean {
         console.log("- Сообщение не должно быть пустым или состоять только из пробелов");
         return message.trim().length > 0;
     }
